6f6b29d2e706222afd322c91f6392327380836f8
heze <895315401@qq.com>
2018-09-02 16:00:52 +0800

    提交

--- cheanda/Classes/Common/Protocol/UINavigationBar/CADNavBackable.swift
+++ cheanda/Classes/Common/Protocol/UINavigationBar/CADNavBackable.swift
@@ -6,4 +6,29 @@
 //  Copyright © 2018年 何泽的mac. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+import NSObject_Rx
+
+protocol CADNavBackable {
+    
+}
+
+extension CADNavBackable where Self : UIViewController {
+    func back(onNext: @escaping ()->Void) {
+        
+        let item = UIBarButtonItem(image: UIImage(named: "list_return_white"), style: .plain, target: nil, action: nil)
+        item.rx.tap.do(onNext: {
+            onNext()
+        }).subscribe().disposed(by: rx.disposeBag)
+        
+        if (navigationItem.leftBarButtonItems?.count ?? 0) == 0 {
+            navigationItem.leftBarButtonItems = [item]
+        } else {
+            var items: [UIBarButtonItem] = [] + navigationItem.leftBarButtonItems!
+            items.append(item)
+            navigationItem.leftBarButtonItems = items
+        }
+    }
+}
--- cheanda/Classes/Common/Protocol/UINavigationBar/CADQRCodeable.swift
+++ cheanda/Classes/Common/Protocol/UINavigationBar/CADQRCodeable.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2018年 何泽的mac. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+import NSObject_Rx
+
+protocol CADQRCodeable {
+    
+}
+
+extension CADQRCodeable where Self : UIViewController {
+    
+    func setting(onNext: @escaping ()->Void) {
+        
+        let item = UIBarButtonItem(image: UIImage(named: "qrcode_scan_light_green"), style: .plain, target: nil, action: nil)
+        item.rx.tap.do(onNext: {
+            onNext()
+        }).subscribe().disposed(by: rx.disposeBag)
+        
+        if (navigationItem.leftBarButtonItems?.count ?? 0) == 0 {
+            navigationItem.leftBarButtonItems = [item]
+        } else {
+            var items: [UIBarButtonItem] = [] + navigationItem.leftBarButtonItems!
+            items.append(item)
+            navigationItem.leftBarButtonItems = items
+        }
+    }
+}
+
+
